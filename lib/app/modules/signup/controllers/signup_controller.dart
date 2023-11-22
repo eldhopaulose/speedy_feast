29ae3d0492ed610d624875c0fb2e05105b07a27e
Eldho Paulose <eldhopaulose0485@gmail.com>
2023-11-22 23:49:08 +0530

    unwanted removed

--- lib/app/modules/signup/controllers/signup_controller.dart
+++ lib/app/modules/signup/controllers/signup_controller.dart
@@ -6,11 +6,10 @@ class SignupController extends GetxController {
 
   final count = 0.obs;
   final isChecked = false.obs;
-  final isCheckedValue = false.obs;
+
   @override
   void onInit() {
     super.onInit();
-    checkAndSetColor();
   }
 
   @override
@@ -27,16 +26,5 @@ class SignupController extends GetxController {
 
   void isChecking(bool? value) {
     isChecked.value = value ?? false;
-    checkAndSetColor(); // Call the method to update the button color
-    print(value);
-  }
-
-  void checkAndSetColor() {
-    if (isChecked.value == true) {
-      isCheckedValue.value == true;
-    }
-    if (isChecked.value == false) {
-      isCheckedValue.value == true;
-    }
   }
 }
